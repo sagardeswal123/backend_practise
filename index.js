ddd6cdf947efec8a45bfce2d1c4b59f6b567341c
sagardeswal123 <sagardeswal798@gmail.com>
2023-07-16 18:12:55 +0530

    changes made

--- index.js
+++ index.js
@@ -9,14 +9,29 @@ app.use(cors());
 
 app.use(express.json());
 
+const connectDB = async () => {
+    try {
+      const conn = await connection;
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+  
+
 app.get("/",async(req,res)=>{
     const data = await dataModel.find().maxTimeMS(40000);
     res.status(200).send(data)
 })
 
-
-
-app.listen(process.env.PORT || 4500,async()=>{
-    await connection
-    console.log('listening on port 4600')
+connectDB().then(() => {
+    app.listen(process.env.PORT || 4500, () => {
+        console.log("listening for requests");
+    })
 })
+
+// app.listen(process.env.PORT || 4500,async()=>{
+//     await connection
+//     console.log('listening on port 4600')
+// })
